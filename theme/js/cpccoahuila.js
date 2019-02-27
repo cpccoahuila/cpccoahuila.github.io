a7c25e846e20b2878bdf86166776f86a100b3c02
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2019-02-27 11:15:38 -0600

    Nuevo tema cpccoahuila-2019-02 aplicado.

---
+++ theme/js/cpccoahuila.js
@@ -0,0 +1,9 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Set style to tables
+    $(document).ready(function () {
+        $("article table").addClass("table table-bordered table-hover");
+    });
+
+})(jQuery); // End of use strict
